f1de698bf4203e15a84e5e7869945e5c2a1ea32d
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-07-04 11:27:53 +0545

    added folderstructure

---
+++ src/sections/about/About.tsx
@@ -0,0 +1,10 @@
+import "./About.css"
+const About = () => {
+  return (
+    <section id='about'>
+        About section
+    </section>
+  )
+}
+
+export default About
---
+++ src/sections/contact/Contact.tsx
@@ -0,0 +1,9 @@
+import "./Contact.css"
+
+const Contact = () => {
+  return (
+    
+  )
+}
+
+export default Contact
