997aecb9672f1e5455df90b2b6b94f60b9812ec6
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-07-06 13:27:13 +0545

    about section

---
+++ src/sections/about/data.tsx
@@ -0,0 +1,11 @@
+
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+
+const data = [
+    { id: 1, icon: <ThumbUpOffAltIcon />, title: "Experience", desc: "3+ Yearly Working" },
+    { id: 2, icon: <ThumbUpOffAltIcon />, title: "Projects", desc: "180+ completed" },
+    { id: 3, icon: <ThumbUpOffAltIcon />, title: "Clents", desc: "70+ happy clients" }
+]
+
+
+export default data;
