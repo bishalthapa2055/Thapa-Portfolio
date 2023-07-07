d704969e7782c369c7fc343d9e4f175399c6679e
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-07-07 17:18:43 +0545

    added services and portfolio

---
+++ src/sections/portfolio/data.tsx
@@ -0,0 +1,12 @@
+
+
+const portfolioImage = 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
+
+const data = [
+    { id: 1, link :"#", image:portfolioImage , title: "Kindship", desc: "Description of the app that you develpped during you time to show some project to other ", role: "Email Tempate" },
+    { id: 2,link :"#" ,image:portfolioImage , title: "Angel Korean Basha", desc: "Angle korean Basha", role: "Node.js && React.js" },
+    { id: 3,  link :"#",image: portfolioImage, title: "Fitness App", desc: "Fintss App", role: "Node.js && React.js"}
+]
+
+
+export default data;
