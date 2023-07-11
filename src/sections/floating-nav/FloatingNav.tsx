a52d101b93443cd56225d6c648d190c4af1efd95
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-07-11 14:15:45 +0545

    floating nav

--- src/sections/floating-nav/FloatingNav.tsx
+++ src/sections/floating-nav/FloatingNav.tsx
@@ -1,8 +1,32 @@
+import { IconButton, Tooltip } from "@mui/material"
 import "./FloatingNav.css"
+import { data } from "./data"
 const FloatingNav = () => {
   return (
-    <section id="floating__nav">
-        <h2>Floatignnn nav</h2>
+
+
+    <section id="floating__nav" className="mobile__section">
+
+      <div className="container floating__container">
+        <ul className="floating__menu">
+          {
+            data.map(({ id, link, title, icon }) => {
+              return (
+                <>
+                  <li key={id}>
+                    <a href={link} className="icon__container">
+                      {icon}
+                    </a>
+                  </li>
+                </>
+              )
+            })
+          }
+        </ul>
+      </div>
+
+
+
     </section>
   )
 }
---
+++ src/sections/floating-nav/data.tsx
@@ -0,0 +1,14 @@
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ChatIcon from '@mui/icons-material/Chat';
+
+export const data = [
+    { id: 1, link: "#", title: "Home", icon: <HomeIcon /> },
+    { id: 2, link: "#about", title: "About", icon: <PersonIcon /> },
+    { id: 3, link: "#services", title: "Services", icon: <MiscellaneousServicesIcon /> },
+    { id: 4, link: "#portfolio", title: "Portfolio", icon: <DashboardIcon /> },
+    { id: 5, link: "#contact", title: "Contact", icon: <ChatIcon /> }
+
+]
